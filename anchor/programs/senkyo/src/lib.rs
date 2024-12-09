6a20053a77d8ed17dc1300a9a26b136c24107b3f
Atsuhiro <atcheri@gmail.com>
2024-12-09 13:49:26 +0100

    feat: add poll creation function
    And the corresponding struct

--- anchor/programs/senkyo/src/lib.rs
+++ anchor/programs/senkyo/src/lib.rs
@@ -12,8 +12,25 @@ pub mod senkyo {
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
 
-        let registration = &mut ctx.accounts.registrations;
-        counter.count = 0;
+        let registrations = &mut ctx.accounts.registrations;
+        registrations.count = 0;
+
+        Ok(())
+    }
+
+    pub fn creat_poll(ctx: Context<CreatePoll>, description: String, start: u64, end: u64) -> Result<()> {
+        if start > end {
+            return Err(ErrorCode::InvalidDate.into())
+        }
+
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        let poll = &mut ctx.accounts.poll;
+        poll.id =  counter.count;
+        poll.description = description;
+        poll.start = start;
+        poll.end = end;
+        poll.candidates = 0;        
 
         Ok(())
     }
@@ -28,7 +45,7 @@ pub struct Initialize<'info> {
         init,
         payer = user,
         space = ANCHOR_DISCRIMINATOR_SIZE + 8,
-        seed = [b"counter"],
+        seeds = [b"counter"],
         bump
     )]
     pub counter: Account<'info, Counter>,
@@ -37,7 +54,7 @@ pub struct Initialize<'info> {
         init,
         payer = user,
         space = ANCHOR_DISCRIMINATOR_SIZE + 8,
-        seed = [b"registrations"],
+        seeds = [b"registrations"],
         bump
     )]
     pub registrations: Account<'info, Registration>,
@@ -55,3 +72,42 @@ pub struct Registration {
     pub count: u64,
 }
 
+#[derive(Accounts)]
+pub struct CreatePoll<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Poll::INIT_SPACE,
+        seeds = [(counter.count + 1).to_le_bytes().as_ref()],       
+         bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    #[account(
+        mut,
+        seeds = [b"counter"],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+
+    pub system_program: Program<'info, System>,
+}
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub start: u64,
+    pub end: u64,
+    pub candidates: u64,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Start date cannot be greater than End date")]
+    InvalidDate
+}
